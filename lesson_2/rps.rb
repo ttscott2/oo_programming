cef5ec97aadd7582e48c322dedb1bcdf3d5a8810
ttscott2 <timothytscott3@gmail.com>
2016-04-27 18:27:01 -0500

    implement choose method

--- lesson_2/rps.rb
+++ lesson_2/rps.rb
@@ -1,11 +1,47 @@
-# Game Orchestration Engine
+class Player
+  attr_accessor :move
 
+
+  def initialize(player_type = :human)
+    @player_type = player_type
+    @move = nil
+  end
+
+  def choose
+    if human?
+      choice = nil
+      loop do
+      puts "Please choose rock, paper, or scissors:"
+      choice = gets.chomp
+      break if ['rock', 'paper', 'scissors'].include? choice
+      puts "Sorry, invalid choice."
+    end
+      self.move = choice
+    else
+      ['rock', 'paper', 'scissors'].sample
+    end
+  end
+
+  def human?
+    @player_type == :human
+  end
+end
+
+# Game Orchestration Engine
 class RPSGame
   attr_accessor :human, :computer
 
   def initialize
-    @human = Player.new
-    @computer = Player.new
+    @human = Player.new(:human)
+    @computer = Player.new(:computer)
+  end
+
+  def display_welcome_message
+    puts "Welcome to Rock, Paper, Scissors!"
+  end
+
+  def display_goodbye_message
+    puts "Thanks for playing Rock, Paper, Scissors. Good bye!"
   end
 
   def play
