04e489baa3dad1007b5cf109bdcc131991e093e3
ttscott2 <timothytscott3@gmail.com>
2016-04-27 18:15:46 -0500

    initial commit

---
+++ lesson_2/rps.rb
@@ -0,0 +1,20 @@
+# Game Orchestration Engine
+
+class RPSGame
+  attr_accessor :human, :computer
+
+  def initialize
+    @human = Player.new
+    @computer = Player.new
+  end
+
+  def play
+    display_welcome_message
+    human.choose
+    computer.choose
+    display_winner
+    display_goodbye_message
+  end
+end
+
+RPSGame.new.play
